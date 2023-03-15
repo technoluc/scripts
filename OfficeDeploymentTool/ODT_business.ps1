e634eb35b89f7f8d0bfb1a27e78a5be2a5d55403
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2023-03-15 15:02:06 +0100

    Update ODT_business.ps1

--- OfficeDeploymentTool/ODT_business.ps1
+++ OfficeDeploymentTool/ODT_business.ps1
@@ -10,19 +10,22 @@ Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=864572" -OutFile
 <!-- This configuration file installs Office 365 Business 64-bit version -->
 <Configuration>
     <!-- Set the Office client edition and update channel -->
-    <Add OfficeClientEdition="64" Channel="Monthly">
+    <Add OfficeClientEdition="64" Channel="Current">
         <!-- Specify the product to install -->
         <Product ID="O365BusinessRetail">
             <!-- Set the language of the installation to Dutch -->
             <Language ID="nl-nl" />
             <!-- Exclude unwanted apps -->
-            <ExcludeApp ID="OneNote" />
+            <ExcludeApp ID="Access" />
             <ExcludeApp ID="Groove" />
             <ExcludeApp ID="InfoPath" />
+            <ExcludeApp ID="Lync" />
+            <ExcludeApp ID="OneNote" />
+            <ExcludeApp ID="Publisher" />
         </Product>
     </Add>
     <!-- Enable updates and set the update channel -->
-    <Updates Enabled="TRUE" Channel="Monthly" />
+    <Updates Enabled="TRUE" Channel="Current" />
     <!-- Force applications to shut down during the installation process -->
     <Property Name="FORCEAPPSHUTDOWN" Value="TRUE" />
     <!-- Suppress user interface display and accept the EULA automatically -->
