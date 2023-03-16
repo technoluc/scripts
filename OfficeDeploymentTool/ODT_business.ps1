a76e8e46336b855262b6667d9118954f950a0eac
TechnoLuc <luc@technoluc.nl>
2023-03-16 11:54:37 +0100

    update script

--- OfficeDeploymentTool/ODT_business.ps1
+++ OfficeDeploymentTool/ODT_business.ps1
@@ -2,8 +2,44 @@
 $setupExe = "C:\ODT\OfficeDeploymentTool.exe"
 $configurationXML = "C:\ODT\Business.xml"
 
+
+# Fetch URL of latest Office 365 Deployment Tool (ODT). 
+function Get-ODTUri {
+    <#
+        .SYNOPSIS
+            Get Download URL of latest Office 365 Deployment Tool (ODT).
+        .NOTES
+            Author: Bronson Magnan
+            Twitter: @cit_bronson
+            Modified by: Marco Hofmann
+            Twitter: @xenadmin
+        .LINK
+            https://www.meinekleinefarm.net/
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param ()
+
+    $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+    try {
+        $response = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
+    }
+    catch {
+        Throw "Failed to connect to ODT: $url with error $_."
+        Break
+    }
+    finally {
+        $ODTUri = $response.links | Where-Object {$_.outerHTML -like "*click here to download manually*"}
+        Write-Output $ODTUri.href
+    }
+}
+
+
+$URL = $(Get-ODTUri)
+
+
 # Download the Office Deployment Tool (ODT)
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=864572" -OutFile $setupExe
+Invoke-WebRequest -Uri $URL -OutFile $setupExe
 
 # Create an XML configuration file
 @"
