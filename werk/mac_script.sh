689aea2f544488652dabf66ca0b7f429b4f547ce
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2023-06-01 16:28:13 +0200

    Update mac_script.sh

--- werk/mac_script.sh
+++ werk/mac_script.sh
@@ -70,7 +70,6 @@ while true; do
     botc "2. Adobe Creative Cloud installeren" $COL_GREEN
     botc "3. Verschillende Handoff-functies van iCloud uitschakelen" $COL_GREEN
     botc "4. Stoppen" $COL_RED
-    echo "Keuze:"
     
     read -r choice
 
