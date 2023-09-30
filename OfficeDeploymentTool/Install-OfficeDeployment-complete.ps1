96dd98b0aeec66243ca635cc118669926cb8b062
technoluc <luc@technoluc.nl>
2023-09-30 20:19:35 +0200

    updates

--- OfficeDeploymentTool/Install-OfficeDeployment-complete.ps1
+++ OfficeDeploymentTool/Install-OfficeDeployment-complete.ps1
@@ -218,6 +218,7 @@ if (Test-Path "C:\Program Files\Microsoft Office") {
   $confirmation = Read-Host "Do you want to run OfficeScrubber? (Y/N, press Enter for Yes)"
   if ($confirmation -eq 'Y' -or $confirmation -eq 'y' -or $confirmation -eq '') {
     Start-Process -FilePath powershell.exe -ArgumentList "Invoke-WebRequest $OfficeRemovalToolUrl -OutFile $OfficeRemovalToolPath; powershell -ExecutionPolicy Bypass $OfficeRemovalToolPath"
+    Read-Host "Pressop Enter to continue..."
     Write-Host "Select [R] Remove all Licenses option in OfficeScrubber." -ForegroundColor Yellow
     Expand-7zArchive -ArchiveUrl $ArchiveUrl -ScrubberPath $ScrubberPath -ScrubberArchive $ScrubberArchive
     Start-Process -Verb runas -FilePath "cmd.exe" -ArgumentList "/C $ScrubberFullPath "
