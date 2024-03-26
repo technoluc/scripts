5fdcd215bd0a17af30684efad816f42aa4c7be7e
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2024-03-26 09:43:50 +0100

    Create mountNASdrives.ps1

---
+++ win/mountNASdrives.ps1
@@ -0,0 +1,12 @@
+net use G: /delete /y
+net use O: /delete /y
+net use R: /delete /y
+net use S: /delete /y
+net use Z: /delete /y
+net use H: /delete /y
+net use X: /delete /y
+net use O: \\nas\Omzet
+net use R: \\nas\Redactie
+net use S: \\nas\Studio
+net use G: \\nas\Home
+net use X: \\nas\Administratie
